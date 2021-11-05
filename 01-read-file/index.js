84302b30ac95d4e0a9864debd94cdc35730be312
manol1 <marsen1491@gmail.com>
2021-11-05 15:39:10 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,4 @@
+const fs = require('fs')
+const path = require('path');
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+readStream.on('data', text => console.log(text));
