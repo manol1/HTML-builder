fe6a231c50a45c8118d29bdc8a210e9906b7a666
manol1 <marsen1491@gmail.com>
2021-11-06 21:05:59 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+  if (err) throw err;
+});
+
+fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+  if (err) throw err;
+  if (files.length > 0) {
+    for (const file of files) {
+      fs.unlink(path.join(__dirname, 'files-copy', file), () => {
+        if (err) throw err;
+      });
+    }
+  }
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), () => {
+        if (err) throw err;
+      });
+    }
+  })
+})
 
